b1b7a57b08281d817630b0670b45af50f84a14f5
Ehize <you@example.com>
2023-09-19 16:51:09 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * reset_to_98 - reset n to 98
- * @n: pointer to the value to be changed
- * Return: n
+* reset_to_98 - reset n to 98
+* @n: pointer to the value to be changed
+* Return: n
 */
-
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
